41ec85d62203b9b729c1526a1c7a983d8b07fd06
C1310033 Thiago de Sousa Pereira <c1310033@interno.bb.com.br>
2020-12-16 20:07:45 -0300

    correção testes não usados

--- src/test/java/br/com/bb/rtc/NativeHelloWorldResourceIT.java
+++
@@ -1,8 +0,0 @@
-package br.com.bb.rtc;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeHelloWorldResourceIT extends HelloWorldResourceTest {
-    // Execute the same tests but in native mode.
-}
